fda469d6b456070da68fa3fdbc07a513d858b200
Tonye Jack <jtonye@ymail.com>
2021-11-24 21:06:17 -0500

    Handle spaces in filenames (#121)
    * Handle spaces in filenames
    
    * Update entrypoint.sh
    
    * Update entrypoint.sh
    
    * Update entrypoint.sh
    
    * Update entrypoint.sh
    
    * Update entrypoint.sh
    
    * Update entrypoint.sh
    
    * Update entrypoint.sh
    
    * Update entrypoint.sh
    
    * Update entrypoint.sh
    
    * Update entrypoint.sh
    
    * Update entrypoint.sh
    
    * Update entrypoint.sh
    
    * Update entrypoint.sh
    
    * Update entrypoint.sh
    
    * Update entrypoint.sh
    
    * Update test.yml
    
    * Update test.yml
    
    * Update test.yml
    
    * Update entrypoint.sh
    
    * Update entrypoint.sh
    
    * Update action.yml
    
    * Update README.md
    
    * Update test.yml

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,26 +10,31 @@ FILES=$(echo "${ALL_FILES[*]}" | awk '{gsub(/ /,"\n"); print $0;}' | awk -v d="|
 
 echo "Checking for file changes: \"${FILES}\"..."
 
-STAGED_FILES+=$(git diff --diff-filter=ACMUXTR --name-only | grep -E "(${FILES})" | awk -v d="|" '{s=(NR==1?s:s d)$0}END{print s}')
+TRACKED_FILES=$(git diff --diff-filter=ACMUXTR --name-only | grep -E "(${FILES})" | awk -v d="|" '{s=(NR==1?s:s d)$0}END{print s}')
 
-# Find unstaged changes
-UNSTAGED_FILES+=$(git status --porcelain | awk '{$1=""; print $0 }' | grep -E "(${FILES})" | awk -v d="|" '{s=(NR==1?s:s d)$0}END{print s}'
+# Find untracked changes
+UNTRACKED_FILES=$(git ls-files --others --exclude-standard | grep -E "(${FILES})" | awk -v d="|" '{s=(NR==1?s:s d)$0}END{print s}')
 
 CHANGED_FILES=""
 
-if [[ -n "$STAGED_FILES" && -n "$UNSTAGED_FILES" ]]; then
-  CHANGED_FILES="$STAGED_FILES|$UNSTAGED_FILES"
-elif [[ -n "$STAGED_FILES" && -z "$UNSTAGED_FILES" ]]; then
-  CHANGED_FILES="$STAGED_FILES"
-elif [[ -n "$UNSTAGED_FILES" && -z "$STAGED_FILES" ]]; then
-  CHANGED_FILES="$UNSTAGED_FILES"
+if [[ -n "$TRACKED_FILES" && -n "$UNTRACKED_FILES" ]]; then
+  CHANGED_FILES="$TRACKED_FILES|$UNTRACKED_FILES"
+elif [[ -n "$TRACKED_FILES" && -z "$UNTRACKED_FILES" ]]; then
+  CHANGED_FILES="$TRACKED_FILES"
+elif [[ -n "$UNTRACKED_FILES" && -z "$TRACKED_FILES" ]]; then
+  CHANGED_FILES="$UNTRACKED_FILES"
 fi
+
+CHANGED_FILES=$(echo "$CHANGED_FILES"  | awk '{gsub(/\|/,"\n"); print $0;}' | sort -u | awk -v d="|" '{s=(NR==1?s:s d)$0}END{print s}')
 
 if [[ -n "$CHANGED_FILES" ]]; then
   echo "Found uncommited changes"
   echo "---------------"
-  printf '%s\n' "$(echo $CHANGED_FILES | awk '{gsub(/\|/," "); print $0;}')"
+  echo "$CHANGED_FILES" | awk '{gsub(/\|/,"\n"); print $0;}'
   echo "---------------"
+
+  CHANGED_FILES=$(echo "$CHANGED_FILES" | awk '{gsub(/\|/,"\n"); print $0;}' | awk -v d="$INPUT_SEPARATOR" '{s=(NR==1?s:s d)$0}END{print s}')
+
   echo "::set-output name=files_changed::true"
   echo "::set-output name=changed_files::$CHANGED_FILES"
 else
