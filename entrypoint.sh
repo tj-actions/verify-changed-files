c4322970b4f055ede155b95586b04562796f83b7
Tonye Jack <jtonye@ymail.com>
2022-08-22 22:57:27 -0600

    feat: switch to use pattern file and fixed bug with excluded paths (#202)

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,15 +2,15 @@
 
 set -e
 
+echo "::group::verify-changed-files"
+
 git config --local core.autocrlf "$INPUT_AUTO_CRLF"
 
-if [[ -n "$INPUT_FILES" ]]; then
-  echo "Checking for file changes: \"${INPUT_FILES}\"..."
-  
-  TRACKED_FILES=$(git diff --diff-filter=ACMUXTRD --name-only | grep -E "(${INPUT_FILES})" | awk -v d="|" '{s=(NR==1?s:s d)$0}END{print s}')
+if [[ -n "$INPUT_FILES_PATTERN_FILE" ]]; then
+  TRACKED_FILES=$(git diff --diff-filter=ACMUXTRD --name-only | grep -x -E -f "$INPUT_FILES_PATTERN_FILE" | awk -v d="|" '{s=(NR==1?s:s d)$0}END{print s}')
 
   # Find untracked changes
-  UNTRACKED_FILES=$(git ls-files --others --exclude-standard | grep -E "(${INPUT_FILES})" | awk -v d="|" '{s=(NR==1?s:s d)$0}END{print s}')
+  UNTRACKED_FILES=$(git ls-files --others --exclude-standard | grep -x -E -f "$INPUT_FILES_PATTERN_FILE" | awk -v d="|" '{s=(NR==1?s:s d)$0}END{print s}')
 else
   TRACKED_FILES=$(git diff --diff-filter=ACMUXTRD --name-only | awk -v d="|" '{s=(NR==1?s:s d)$0}END{print s}')
 
@@ -32,9 +32,6 @@ CHANGED_FILES=$(echo "$CHANGED_FILES"  | awk '{gsub(/\|/,"\n"); print $0;}' | so
 
 if [[ -n "$CHANGED_FILES" ]]; then
   echo "Found uncommited changes"
-  echo "---------------"
-  echo "$CHANGED_FILES" | awk '{gsub(/\|/,"\n"); print $0;}'
-  echo "---------------"
 
   CHANGED_FILES=$(echo "$CHANGED_FILES" | awk '{gsub(/\|/,"\n"); print $0;}' | awk -v d="$INPUT_SEPARATOR" '{s=(NR==1?s:s d)$0}END{print s}')
 
@@ -45,4 +42,4 @@ else
   echo "::set-output name=files_changed::false"
 fi
 
-exit 0;
+echo "::endgroup::"
