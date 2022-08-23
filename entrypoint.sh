3efe8460199003c8de3d30e0fdb1d031a536d5a2
Tonye Jack <jtonye@ymail.com>
2022-08-22 22:50:10 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,11 @@
 
 set -e
 
+echo "::group::verify-changed-files"
+
 git config --local core.autocrlf "$INPUT_AUTO_CRLF"
 
 if [[ -n "$INPUT_FILES_PATTERN_FILE" ]]; then
-  echo "Checking for file changes: \"${INPUT_FILES}\"..."
-  
   TRACKED_FILES=$(git diff --diff-filter=ACMUXTRD --name-only | grep -x -E -f "$INPUT_FILES_PATTERN_FILE" | awk -v d="|" '{s=(NR==1?s:s d)$0}END{print s}')
 
   # Find untracked changes
@@ -31,18 +31,15 @@ fi
 CHANGED_FILES=$(echo "$CHANGED_FILES"  | awk '{gsub(/\|/,"\n"); print $0;}' | sort -u | awk -v d="|" '{s=(NR==1?s:s d)$0}END{print s}')
 
 if [[ -n "$CHANGED_FILES" ]]; then
-  echo "Found uncommited changes"
-  echo "---------------"
-  echo "$CHANGED_FILES" | awk '{gsub(/\|/,"\n"); print $0;}'
-  echo "---------------"
+  echo "::debug::Found uncommited changes"
 
   CHANGED_FILES=$(echo "$CHANGED_FILES" | awk '{gsub(/\|/,"\n"); print $0;}' | awk -v d="$INPUT_SEPARATOR" '{s=(NR==1?s:s d)$0}END{print s}')
 
   echo "::set-output name=files_changed::true"
   echo "::set-output name=changed_files::$CHANGED_FILES"
 else
-  echo "No changes found."
+  echo "::debug::No changes found."
   echo "::set-output name=files_changed::false"
 fi
 
-exit 0;
+echo "::endgroup::"
