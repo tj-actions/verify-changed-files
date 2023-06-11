2d62d9f157cc854b0be88b3b6e8df32eaefda1d4
Tonye Jack <jtonye@ymail.com>
2023-06-10 22:00:40 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,23 +22,23 @@ if [[ -n "$INPUT_FILES_PATTERN_FILE" ]]; then
 
   # Find untracked changes
   # shellcheck disable=SC2086
-  UNTRACKED_FILES=$(git status $GIT_STATUS_EXTRA_ARGS | awk '{print $NF}' | { grep -x -E -f "$INPUT_FILES_PATTERN_FILE" || true; } | awk -v d="|" '{s=(NR==1?s:s d)$0}END{print s}')
+  UNTRACKED_OR_IGNORED_FILES=$(git status $GIT_STATUS_EXTRA_ARGS | awk '{print $NF}' | { grep -x -E -f "$INPUT_FILES_PATTERN_FILE" || true; } | awk -v d="|" '{s=(NR==1?s:s d)$0}END{print s}')
 else
   TRACKED_FILES=$(git diff --diff-filter=ACMUXTRD --name-only | awk -v d="|" '{s=(NR==1?s:s d)$0}END{print s}')
 
   # Find untracked changes
   # shellcheck disable=SC2086
-  UNTRACKED_FILES=$(git status $GIT_STATUS_EXTRA_ARGS | awk '{print $NF}' | awk -v d="|" '{s=(NR==1?s:s d)$0}END{print s}')
+  UNTRACKED_OR_IGNORED_FILES=$(git status $GIT_STATUS_EXTRA_ARGS | awk '{print $NF}' | awk -v d="|" '{s=(NR==1?s:s d)$0}END{print s}')
 fi
 
 CHANGED_FILES=""
 
-if [[ -n "$TRACKED_FILES" && -n "$UNTRACKED_FILES" ]]; then
-  CHANGED_FILES="$TRACKED_FILES|$UNTRACKED_FILES"
-elif [[ -n "$TRACKED_FILES" && -z "$UNTRACKED_FILES" ]]; then
+if [[ -n "$TRACKED_FILES" && -n "$UNTRACKED_OR_IGNORED_FILES" ]]; then
+  CHANGED_FILES="$TRACKED_FILES|$UNTRACKED_OR_IGNORED_FILES"
+elif [[ -n "$TRACKED_FILES" && -z "$UNTRACKED_OR_IGNORED_FILES" ]]; then
   CHANGED_FILES="$TRACKED_FILES"
-elif [[ -n "$UNTRACKED_FILES" && -z "$TRACKED_FILES" ]]; then
-  CHANGED_FILES="$UNTRACKED_FILES"
+elif [[ -n "$UNTRACKED_OR_IGNORED_FILES" && -z "$TRACKED_FILES" ]]; then
+  CHANGED_FILES="$UNTRACKED_OR_IGNORED_FILES"
 fi
 
 CHANGED_FILES=$(echo "$CHANGED_FILES"  | awk '{gsub(/\|/,"\n"); print $0;}' | sort -u | awk -v d="|" '{s=(NR==1?s:s d)$0}END{print s}')
